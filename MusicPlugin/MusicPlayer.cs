e8112a8a1f2f2c59f1e71821434ad563f88a4fe6
Slendys <Slendys@DESKTOP-RO1L43F>
2024-12-14 13:59:59 +0300

    add url load support

--- MusicPlugin/MusicPlayer.cs
+++ MusicPlugin/MusicPlayer.cs
@@ -6,10 +6,11 @@ namespace MusicPlugin
 {
     public class MusicHandler : MonoBehaviour
     {
-        private static GameObject IntroObject;
         private static string playerMusicPath = Directory.GetCurrentDirectory().Replace("\\", "/") + "/Plugins/MusicPlugin/Music";
         private string musicPath = Directory.GetCurrentDirectory().Replace("\\", "/") + "/Plugins/MusicPlugin/";
         private string[] musicList;
+        private string musicName = "None";
+        private string musicUrl;
         private AudioSource musicSource;
         private Vector2 scrollPosition;
         private bool viewPlayer = false;
@@ -42,28 +43,36 @@ namespace MusicPlugin
             {
                 Cursor.visible = true;
                 Cursor.lockState = CursorLockMode.None;
-                GUILayout.BeginArea(new Rect(Screen.width / 2 - 150, Screen.height / 2 - 100, 300, 200));
+                GUILayout.BeginArea(new Rect(Screen.width / 2 - 150, Screen.height / 2 - 100, 310, 300));
                 GUILayout.BeginVertical("box");
+                GUILayout.Box($"Current:{musicName} Volume:{musicSource.volume}");
                 scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(300), GUILayout.Height(150));
                 if (musicList != null)
                 {
-                    GUILayout.Box($"Volume: {musicSource.volume}");
                     foreach (var Music in musicList)
                     {
-                        GUILayout.BeginHorizontal();
+                        GUILayout.BeginVertical();
                         GUILayout.Box(Path.GetFileName(Music));
                         if (GUILayout.Button("Play"))
                         {
                             gameObject.GetComponent<MusicHandler>().LoadAndPlayAudio(musicSource, Music);
+                            musicName = Path.GetFileName(Music);
                         }
                         GUILayout.EndHorizontal();
                     }
                 }
                 GUILayout.EndScrollView();
+                musicUrl = GUILayout.TextField(musicUrl);
                 musicSource.volume = GUILayout.HorizontalSlider(musicSource.volume, 0, 1);
+                if (GUILayout.Button("Load by URL"))
+                {
+                    LoadAndPlayUrl(musicSource, musicUrl);
+                    musicName = Path.GetFileName(Path.GetFileName(musicUrl));
+                }
                 if (GUILayout.Button("Stop"))
                 {
                     musicSource.Stop();
+                    musicName = "None";
                 }
                 GUILayout.EndVertical();
                 GUILayout.EndArea();
@@ -72,6 +81,27 @@ namespace MusicPlugin
         public void LoadAndPlayAudio(AudioSource _audioSource, string FileName = "mainmusic.mp3")
         {
             StartCoroutine(LoadAndPlayAudioCoroutine(_audioSource));
+        }
+        public void LoadAndPlayUrl(AudioSource _audioSource, string _URL)
+        {
+            StartCoroutine(LoadAndPlayUrlCoroutine(_audioSource, _URL));
+        }
+        private IEnumerator LoadAndPlayUrlCoroutine(AudioSource audioSource, string URL)
+        {
+            using (var www = new WWW(URL))
+            {
+                yield return www;
+
+                if (!string.IsNullOrEmpty(www.error))
+                {
+                    Debug.LogError("Error load File: " + www.error);
+                }
+                else
+                {
+                    audioSource.clip = www.GetAudioClip(false, true, AudioType.MPEG);
+                    audioSource.Play();
+                }
+            }
         }
         private IEnumerator LoadAndPlayAudioCoroutine(AudioSource audioSource, string FileName = "mainmusic.mp3")
         {
@@ -98,12 +128,12 @@ namespace MusicPlugin
             }
         }
     }
-    [ContentWarningPlugin("MusicPlugin", "1.2.1", false)]
+    [ContentWarningPlugin("MusicPlugin", "2.0", false)]
     public class PluginTy
     {
         static PluginTy()
         {
-            Debug.Log("MusicPlugin load.");
+            Debug.Log("MusicPlugin load");
         }
     }
     [HarmonyPatch(typeof(IntroScreenAnimator))]
